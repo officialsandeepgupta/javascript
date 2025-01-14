97dc6a8b1737a5ac31cf526a7526b6e2c7c3843a
Sandeep Gupta <officialsandeepgupta@gmail.com>
2025-01-14 13:05:13 +0000

    variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,7 @@
+const accountId=10001;
+let FullName="Sasndeep Gupta";
+var Email="sandeep@google.com";
+
+
+console.log(`User Id: ${accountId}, User Name: ${FullName}, User Email: ${Email}`);
+console.table([accountId,FullName,Email]);
